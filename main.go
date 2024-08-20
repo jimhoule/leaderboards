96749dd085c12a22cc31c591b3465e9b95baca82
Jimmy Houle <jimhoule09@gmail.com>
2024-08-20 17:13:33 -0400

    initializing players module

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"main/games"
+	"main/players"
 	"main/router"
 	"net/http"
 	"os"
@@ -12,15 +13,20 @@ import (
 )
 
 func main() {
+	// Envs
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Println("error loading envs", err)
 	}
 
+	// Router
 	mainRouter := router.Get()
 
+	// Modules
 	games.Init(mainRouter)
+	players.Init(mainRouter)
 
+	// Http server
 	server := &http.Server{
 		Addr: fmt.Sprintf("%s:%s", os.Getenv("HTTP_URL"), os.Getenv("HTTP_PORT")),
 		Handler: mainRouter,
